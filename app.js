20693fc5c2ff7855ed56c21b921332f52e3f925d
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-08-13 17:28:58 +0900

    canvas의 너비, 높이는 js에서도 줘야한다

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("hello");
+const canvas = document.querySelector("canvas");
+canvas.width = 800;
+canvas.height = 800;
+const ctx = canvas.getContext("2d");
+
+ctx.rect(50, 50, 50, 50);
+ctx.stroke();
