23cf82f6d16c38ee7e13803f6cd6e4dcc82e7b82
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-08-13 20:37:33 +0900

    집 만들기

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ canvas.width = 800;
 canvas.height = 800;
 const ctx = canvas.getContext("2d");
 
-ctx.moveTo(50, 50);
-ctx.lineTo(100, 50);
-ctx.lineTo(100, 100);
-ctx.fillStyle = "aqua";
+/*집만들기*/
+ctx.fillRect(150, 400, 100, 300);
+ctx.fillRect(450, 400, 100, 300);
+
+ctx.lineWidth = 2;
+ctx.strokeStyle = "brown";
+ctx.strokeRect(300, 500, 100, 200);
+
+ctx.fillRect(150, 400, 400, 50);
+
+ctx.moveTo(150, 400);
+ctx.lineTo(350, 200);
+ctx.lineTo(550, 400);
+ctx.fillStyle = "green";
 ctx.fill();
