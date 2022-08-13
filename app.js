ac38f1f06bf5f64d914c18df1579d1c4d4e585c7
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-08-13 17:44:06 +0900

    fill & strock 에 style주기 / New Path 만들기
    beginPath()

--- app.js
+++ app.js
@@ -7,4 +7,11 @@ ctx.rect(50, 50, 50, 50);
 ctx.rect(100, 100, 50, 50);
 ctx.rect(150, 150, 50, 50);
 
+ctx.fillStyle = "red";
 ctx.fill();
+
+ctx.beginPath();
+ctx.rect(200, 200, 50, 50);
+ctx.rect(250, 250, 50, 50);
+ctx.strokeStyle = "gold"
+ctx.stroke();
