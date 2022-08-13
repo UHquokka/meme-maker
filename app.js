7a36d7c4c67d372d8fde6976ea4c997260ff56c4
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-08-13 20:55:21 +0900

    사람만들기, arc - Math.PI

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ canvas.width = 800;
 canvas.height = 800;
 const ctx = canvas.getContext("2d");
 
-/*집만들기*/
-ctx.fillRect(150, 400, 100, 300);
-ctx.fillRect(450, 400, 100, 300);
+/*사람 만들기*/
+ctx.fillRect(200, 200, 15, 100);
+ctx.fillRect(400, 200, 15, 100);
+ctx.fillRect(260, 200, 100, 200);
 
-ctx.lineWidth = 2;
-ctx.strokeStyle = "brown";
-ctx.strokeRect(300, 500, 100, 200);
+ctx.arc(300, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
 
-ctx.fillRect(150, 400, 400, 50);
-
-ctx.moveTo(150, 400);
-ctx.lineTo(350, 200);
-ctx.lineTo(550, 400);
+ctx.beginPath();
 ctx.fillStyle = "green";
+ctx.arc(270, 100, 10, Math.PI, 2 * Math.PI);
+ctx.arc(310, 100, 10, Math.PI, 2 * Math.PI);
 ctx.fill();
