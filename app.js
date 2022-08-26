19e5e39abdbae8b765ae8510689be4d035bc8bee
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-08-26 22:01:34 +0900

    Distroy와 Eraser 버튼 만들기

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+const eraserBtn = document.getElementById("eraser-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
 const lineWidth = document.getElementById("line-width");
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
+
 function onMove(event) {
     if (isPainting) {
         ctx.lineTo(event.offsetX, event.offsetY);
@@ -36,12 +45,45 @@ function onColorChangeAll(event) {
     ctx.fillStyle = colorPick;
 }
 
+function onModeClick() {
+    if (isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "FIll";
+    }
+    else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+
+}
+function onCanvasClick() {
+    if (isFilling) {
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+
+}
+function onDestroyClick() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+}
+function onEaraserClick() {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 //마우스가 캔버스 밖으로 나갔을 때 mouseup이 감지 안되는 것을 방지//
+canvas.addEventListener("click", onCanvasClick);
+
 lineWidth.addEventListener("change", onColorChangeAll);
 color.addEventListener("change", onColorChangeAll);
 
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEaraserClick);
